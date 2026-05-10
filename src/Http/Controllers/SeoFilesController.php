a9211f962ea55b44dc974ca17edc320a4e9baa55
Nirjon Roy <roynirjon18@gmail.com>
2026-05-10 18:29:20 +0600

    Add SEO static file handling and configuration options for robots.txt, llms.txt, and security.txt

---
+++ src/Http/Controllers/SeoFilesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Nirjon\LaravelSeo\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+
+class SeoFilesController extends Controller
+{
+    public function robots()
+    {
+        return response(config('seo.files.robots', ''), 200, ['Content-Type' => 'text/plain']);
+    }
+
+    public function llms()
+{
+    $content = config('seo.files.llms_txt');
+    
+    return response($content, 200)->header('Content-Type', 'text/plain');
+}
+
+    public function security()
+    {
+        return response(config('seo.files.security', ''), 200, ['Content-Type' => 'text/plain']);
+    }
+}
