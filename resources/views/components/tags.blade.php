44127a8694a451d970439d3a9d640ec0f75ca355
Nirjon Roy <roynirjon18@gmail.com>
2026-05-11 18:23:49 +0600

    Enhance SEO functionality by adding fallback configurations and updating SEO tags in views; refactor SeoTags component to accept model

--- resources/views/components/tags.blade.php
+++ resources/views/components/tags.blade.php
@@ -1,5 +1,26 @@
-@foreach($tags as $tag)
-    {!! $tag !!}
+@php
+    $siteName = config('seo.fallbacks.site_name', env('APP_NAME', 'Laravel'));
+    $title = config('seo.fallbacks.default_title', 'Welcome');
+    $description = config('seo.fallbacks.default_description', 'Default description');
+
+    if (isset($model) && method_exists($model, 'getSeoTitle')) {
+        $title = $model->getSeoTitle();
+        $description = optional($model->seo)->meta_description ?? $description;
+    }
+@endphp
+
+<title>{{ $title }}</title>
+<meta name="description" content="{{ $description }}">
+
+<meta property="og:title" content="{{ $title }}">
+<meta property="og:description" content="{{ $description }}">
+<meta property="og:site_name" content="{{ $siteName }}">
+
+<meta name="twitter:title" content="{{ $title }}">
+<meta name="twitter:description" content="{{ $description }}">
+
+@foreach(config('seo.meta', []) as $key => $value)
+    <meta name="{{ $key }}" content="{{ $value }}">
 @endforeach
 
-{!! $schemas !!}
+{!! $schemas ?? '' !!}
